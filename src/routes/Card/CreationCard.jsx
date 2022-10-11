2b5656cb2f941c8f90f25bbf5b3108aff32628df
Fernando <lucvs177@gmail.com>
2022-10-11 20:48:17 -0300

    se agrego formulario de clima

--- src/routes/Card/CreationCard.jsx
+++ src/routes/Card/CreationCard.jsx
@@ -1,7 +1,34 @@
+import { useForm } from "react-hook-form";
+
 const  NewCard = () => {
+    
+    
+    const {register, handleSubmit, formState: {errors}}=useForm();
+    const onSubmit = (data) => {
+        console.log(data)
+        
+      }
     return (
         <>
         <h1>Formulario De creacion de targeta</h1>
+    <div className="Contenedor">
+      <h1>Formularios</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type='password'
+            placeholder="Contraseña"
+            {...register(
+              'password',
+              
+            )}
+          />
+          <p>{errors.username?.message}</p>
+          <button type='submit'>Enviar</button>
+
+          
+      </form>
+      
+    </div>
         </>
     );
 };
