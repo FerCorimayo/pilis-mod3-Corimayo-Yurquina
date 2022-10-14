d5efc92f7732f0bea01cba83fa4bb5de42545837
Cristian Yurquina <c.d.yurquina@gmail.com>
2022-10-14 04:18:03 -0300

    logo y estilo de barra

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,13 +1,13 @@
 import { useContext } from 'react';
 import { CardsContext } from '../../context/CardContex'
-import Cardses from '../../components/Cards'
+import Cards from '../../components/Cards'
 const Home = () => {
     const { cardsInfo } = useContext(CardsContext);
     console.log(cardsInfo)
     return (
         <>
         <h3>Inicio</h3>
-        <Cardses cards={cardsInfo}/>
+        <Cards cards={cardsInfo}/>
         </>
     );
 };
--- src/routes/Navigation/Navigation.jsx
+++ src/routes/Navigation/Navigation.jsx
@@ -1,11 +1,21 @@
 import { Link, Outlet } from 'react-router-dom';
-
+import pageLogo from '../../assets/images/logo.png';
+import './Navigation.css';
 
 const Navigation = () => {
     return (
         <>
-        <Link to='newCard'>Nueva Targeta</Link>
-        <h1>Navegacion</h1>
+        <div className='navigation'>
+            <Link className='logo-container' to='/'>
+            <img src={pageLogo} alt='Logo' className='logo' />
+            </Link>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='newCard'>
+                    Nueva Targeta
+                </Link>
+{/*                 <h1>Navegacion</h1> */}
+            </div>
+        </div>
         <Outlet />
         </>
 
