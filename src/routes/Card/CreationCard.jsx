7bb1ab1ec8bba4086da5d9ae058573edb421693a
Fernando <lucvs177@gmail.com>
2022-10-13 21:56:45 -0300

    llamada a la api para agregar a contex

--- src/routes/Card/CreationCard.jsx
+++ src/routes/Card/CreationCard.jsx
@@ -1,28 +1,86 @@
 import { useForm } from "react-hook-form";
+import { getCards } from "./servce";
+import { useContext, useEffect, useState } from 'react';
+import { CardsContext} from './context/CardContext'
+import { useNavigate } from 'react-router-dom';
+
 
 const  NewCard = () => {
-    
-    
+    const navigate = useNavigate()
+    const[coordenadas,setCooredenadas]=useState([])
+
+    const { cardsInfo,setCards } = useContext(CardsContext)
+
+
+
+    /*https://api.open-meteo.com/v1/forecast?current_weather=true&latitude=${-24.255107931678122}&longitude=${-65.21823581670436}&timezone=America/Argentina/Jujuy*/
     const {register, handleSubmit, formState: {errors}}=useForm();
+    
+
     const onSubmit = (data) => {
         console.log(data)
-        
+        const coords = [parseFloat(data.text),parseFloat(data.text2),data.text,data.tex2,data.text3]
+        setCooredenadas(coords);
+        console.log(cardsInfo)
+        navigate('/')
       }
+
+
+
+      useEffect(()=> {
+        console.log(cardsInfo)
+        if(coordenadas.length>1){
+          getCards(coordenadas[0],coordenadas[1])
+          .then((data) => {
+            const informacion={id:cardsInfo.length+1,lugar: coordenadas[4],latitude: data.latitude,longitude: data.longitude,temperatura: data.current_weather.temperature,velocidadViento: data.current_weather.windspeed}
+            setCooredenadas([0])
+            setCards([...cardsInfo,informacion]);
+
+        })
+        .catch((err) => console.log(err));
+
+
+      }
+    }, [coordenadas])
+
+
     return (
         <>
         <h1>Formulario De creacion de targeta</h1>
     <div className="Contenedor">
       <h1>Formularios</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
+
           <input
-            type='password'
-            placeholder="Contraseña"
+            type='text'
+            placeholder="Nombre de lugar"
             {...register(
-              'password',
+              'text3',
               
             )}
           />
-          <p>{errors.username?.message}</p>
+          <p>{errors.text3?.message}</p>
+          <input className="imput-coord"
+            type='text'
+            placeholder="latitud"
+            {...register(
+              'text',
+              
+            )}
+          />
+          <p>{errors.text?.message}</p>
+
+          <input className="imput-coord"
+            type='text'
+            placeholder="longitud"
+            {...register(
+              'text2',
+              
+            )}
+          />
+          <p>{errors.text2?.message}</p>
+
+
           <button type='submit'>Enviar</button>
 
           
